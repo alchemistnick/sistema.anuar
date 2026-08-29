638b2782a1af027ed235eb2586db043cf713c0bc
alchemistnick <91359051+alchemistnick@users.noreply.github.com>
2026-08-28 23:40:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(
     layout="wide",
 )
 
-API_URL = "https://script.google.com/macros/s/AKfycbzbPKv-DdELuHy2FugQ3s6ZENEU1gUwFiDaK05r2t6qaqaUND7bmNqfwOsePnPNYb_hJQ/exec"
+API_URL = "https://script.google.com/macros/s/AKfycbyVCSQXsfeI-6nbzBXmQwT9k-j8efcS9QE51m6QN3ZRrHCI_zb5CiwnuF9Kc50Vr6rUKQ/exec"
 
 
 def api_get(action, params=""):
